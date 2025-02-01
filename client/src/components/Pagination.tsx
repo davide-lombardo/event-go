ee1134bc038e6ef7d6421739b3fb43ad7b6b26a0
davide.lombardo <dav1de92@hotmail.it>
2025-02-01 18:02:23 +0100

    new structure

--- client/src/components/Pagination.tsx
+++ client/src/components/Pagination.tsx
@@ -74,9 +74,9 @@ const Pagination: React.FC<PaginationProps> = ({ hasMore }) => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <line x1="19" y1="12" x2="5" y2="12"></line>
           <polyline points="12 19 5 12 12 5"></polyline>
@@ -93,9 +93,9 @@ const Pagination: React.FC<PaginationProps> = ({ hasMore }) => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <line x1="5" y1="12" x2="19" y2="12"></line>
           <polyline points="12 5 19 12 12 19"></polyline>
