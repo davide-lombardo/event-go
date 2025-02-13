9858ea5ca32c6de150dbabf5639314fccb35574d
davide-lombardo <davide.lombardo.92@gmail.com>
2025-02-13 19:52:21 +0100

    db config update

--- server/prisma/migrations/20250213184445_first_one/migration.sql
+++ server/prisma/migrations/20250213184445_first_one/migration.sql
@@ -6,9 +6,10 @@ CREATE TABLE "Event" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "location" TEXT NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
     "description" TEXT NOT NULL,
     "link" TEXT NOT NULL,
-    "tags" TEXT[],
     "paid" BOOLEAN NOT NULL,
     "userImage" TEXT NOT NULL,
     "userName" TEXT NOT NULL,
@@ -24,19 +25,26 @@ CREATE TABLE "Event" (
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "displayName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "photoURL" TEXT,
-    "bio" TEXT,
     "following" TEXT[] DEFAULT ARRAY[]::TEXT[],
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
+CREATE INDEX "Event_name_idx" ON "Event"("name");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- AddForeignKey
 ALTER TABLE "Event" ADD CONSTRAINT "Event_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
